ce256ec9946f4ccbd3ce7c4b33356f39fe487eda
Matheus <mateus.morais@a3solutions.com.br>
2019-10-09 12:35:15 -0300

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,31 @@
 import 'package:fisc/page/login/login_page.dart';
 import 'package:fisc/utils/event_bus.dart';
 import 'package:flutter/material.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _debugLabelString = "";
+  String _emailAddress;
+  String _externalUserId;
+  bool _enableConsentButton = false;
+
+  // CHANGE THIS parameter to true if you want to test GDPR privacy consent
+  bool _requireConsent = true;
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -28,6 +47,21 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+
+  Future<void> initPlatformState() async {
+    OneSignal.shared.init(
+        "762a9245-09ae-497c-8df1-3924d1fc1b4d",
+        iOSSettings: {
+          OSiOSSettings.autoPrompt: false,
+          OSiOSSettings.inAppLaunchUrl: true
+        }
+    );
+    OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.notification);
+
+
+  }
+
+
 }
 
 
--- lib/page/login/login_api.dart
+++ lib/page/login/login_api.dart
@@ -7,39 +7,37 @@ import 'package:http/http.dart' as http;
 
 class LoginApi {
   static Future<ApiResponse<UsuarioResponse>> login(String login, String Senha) async {
-   try{
-     var url =
-         'services/usuario/autenticacao';
+    try{
+      var url =
+          '/services/usuario/autenticacao';
 
-     Map params = {
-       'login': login,
-       'senha': Senha,
-     };
+      Map params = {
+        'login': login,
+        'senha': Senha,
+      };
 
-     var body = json.encode(params);
+      var body = json.encode(params);
 
-     var response = await http.post(url,
-         headers: {"Content-Type": "application/json"}
-         , body: body);
-     print('Response status: ${response.statusCode}');
-     print('Response body: ${response.body}');
+      var response = await http.post(url,
+          headers: {"Content-Type": "application/json"}
+          , body: body);
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
 
-     /*  Map mapResponse = jsonDecode(response.body);
-    mapResponse.forEach((k,v)=> print(Rotina.fromJson(v).idProcessamento));*/
 
-     Map mapResponse = jsonDecode(response.body);
+      Map mapResponse = jsonDecode(response.body);
 
-     if(response.statusCode==200){
-       final user = UsuarioResponse.fromJson(mapResponse);
+      if(response.statusCode==200){
+        final user = UsuarioResponse.fromJson(mapResponse);
         user.user.save();
         User user2 = await User.get();
 
-       return ApiResponse.ok(user);
-     }
-     return ApiResponse.error(mapResponse["error"]);
-   }catch(error, exception){
+        return ApiResponse.ok(user);
+      }
+      return ApiResponse.error(mapResponse["error"]);
+    }catch(error, exception){
       print("erro no login");
       return ApiResponse.error("Não foi possível fazer o login");
-   }
+    }
   }
 }
