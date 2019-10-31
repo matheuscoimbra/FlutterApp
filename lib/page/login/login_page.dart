a43e2ce6fbdbae05eca4e43e3dfea6d521cf612b
Matheus <mateus.morais@a3solutions.com.br>
2019-10-31 14:09:54 -0300

    ok

--- lib/page/login/login_page.dart
+++ lib/page/login/login_page.dart
@@ -66,47 +66,133 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  _body() {
-    return Form(
-      key: _formKey,
-      child: Container(
-        padding: EdgeInsets.all(16),
-        child: ListView(
-         children: <Widget>[
-           SizedBox(
-             width: 160,
-             height: 160,
-             child: Image.network( "https://media.licdn.com/dms/image/C4E0BAQEz2vTBRFh6Qw/company-logo_200_200/0?e=2159024400&v=beta&t=eet2EKr0GEwEujtH4YeQDNIvFwgfnJOmSfh73m4zXlY"),
-           ),
-           SizedBox(
-             height: 20,
-           ),
-           AppText("Login", "Digite o login", controller: _tLogin, validator: (String text){
-            if(text.isEmpty){
-              return "Digite o login";
-            }
-            return null;
-          }, textInputAction: TextInputAction.next,nextNode: _focusSenha),
-           SizedBox(
-             height: 10,
-           ),
-           AppText("Senha","Digite a senha", password: true, controller: _tSenha, validator: _validaSenha,focusNode: _focusSenha),
-           SizedBox(
-             height: 10,
-           ),
-           StreamBuilder<bool>(
-             stream: _streamControler.stream,
-             builder: (context,snapshot){
-                return AppButton("Login", onPressed: _onClickLogin, showProgress: snapshot.data!=null?snapshot.data:false,);
-             },
-           ),
-
-
-
-         ],
+  _body() { return Form(
+    key: _formKey,
+    child: Stack(
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: NetworkImage( "https://www.policiacivil.ma.gov.br/wp-content/uploads/2017/04/logo-governoma-2015.png"),
+                  fit: BoxFit.fitWidth,
+                  alignment: Alignment.topCenter
+              )
+          ),
         ),
-      ),
-    );
+        Container(
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.only(top: 270),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.white,
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(23),
+            child: ListView(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+                  child: Container(
+                    color: Color(0xfff5f5f5),
+                    child: TextFormField(
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'SFUIDisplay'
+                      ),
+                      controller: _tLogin,
+                      validator: (String text){
+                        if(text.isEmpty){
+                          return "Digite o login";
+                        }
+                        return null;
+                      },
+                      textInputAction: TextInputAction.next,
+                      onFieldSubmitted: (String text) {
+                        if ( _focusSenha != null) {
+                          FocusScope.of(context).requestFocus(_focusSenha);
+                        }
+                      },
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Usuário',
+                          prefixIcon: Icon(Icons.person_outline),
+                          labelStyle: TextStyle(
+                              fontSize: 15
+                          )
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Color(0xfff5f5f5),
+                  child: TextFormField(
+                    obscureText: true,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'SFUIDisplay'
+                    ),
+                    controller: _tSenha,
+                    validator: (String text){
+                      if(text.isEmpty){
+                        return "Digite a senha";
+                      }
+                      if(text.length<5){
+                        return "Senha precisa ter pelo menos 5 digitos";
+                      }
+                      return null;
+                    },
+                    focusNode: _focusSenha,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Senha',
+                        prefixIcon: Icon(Icons.lock_outline),
+                        labelStyle: TextStyle(
+                            fontSize: 15
+                        )
+                    ),
+                  ),
+                ),
+                StreamBuilder<bool>(
+                  stream: _streamControler.stream,
+                  builder: (context,snapshot){
+                    return  Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: MaterialButton(
+                        onPressed: _onClickLogin,//
+                        // since this is only a UI app
+                        child:snapshot.data!=null? Center(
+                          child: CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                          ),
+                        ): Text('LOGIN',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontFamily: 'SFUIDisplay',
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        color: Color(0xffff2d55),
+                        elevation: 0,
+                        minWidth: 400,
+                        height: 50,
+                        textColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)
+                        ),
+                      ),
+                    );
+                  },
+                ),
+
+
+              ],
+            ),
+          ),
+        )
+      ],
+    ),
+  );
+
   }
 
 
